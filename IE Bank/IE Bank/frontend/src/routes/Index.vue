369a67a7a68079614135004cc13827d2aa124d1f
ptorrado2003 <152506080+ptorrado2003@users.noreply.github.com>
2024-10-18 13:27:56 -0400

    Add files via upload

--- IE Bank/IE Bank/frontend/src/routes/Index.vue
+++ IE Bank/IE Bank/frontend/src/routes/Index.vue
@@ -9,7 +9,7 @@ import Functions from '../components/Functions.vue';
         <div class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
             <div class="mx-auto max-w-3xl text-center">
                 <h1
-                    class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+                    class="text-5xl">
                     Welcome to Fintech.
 
                     <span class="sm:block">Your secure bank </span>
